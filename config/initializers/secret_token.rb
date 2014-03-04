19c43fc005321614ff59e5291e079a48c3f874b5
Patrick McCarver <pmmhy4@mst.edu>
2014-03-03 22:34:15 -0600

    AWDWR Depot Scaffold init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'a8f6e87f4bc6e4bcafe3dec838c12cd4c8076c6d7be1a4481a1bcada53a829c28f3a643ae6c2e94ebf57c1984c98e16f0fd9b8f9e226c0ab18d2d862bc319e5f'
